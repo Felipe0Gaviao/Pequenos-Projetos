603409805c6cc5cd88b143f29925a77038d1bd2f
BatataSteve <batatasteve304@gmail.com>
2023-06-19 21:07:54 -0300

    Primeiro Pequeno Projeto

---
+++ Python/Quizz/Quizz.py
@@ -0,0 +1,29 @@
+print("Bem vindo ao Quizz")
+
+iniciar_jogo = input("Você gostaria de jogar? ")
+
+if iniciar_jogo == "sim":
+    print("Okay, então vamos começar o jogo")
+else:
+    quit()
+
+def questao(pergunta,resposta):
+
+    user = input(f'{pergunta}\n')
+    resposta = str(resposta)
+
+    if user == resposta:
+        print(f'Acertou! A resposta é {resposta}')
+    else:
+        print(f'Errou! A resposta certa era {resposta}')
+
+questao("Em que ano o Titanic afundou no Oceano Atlântico em 15 de abril, em sua viagem inaugural de Southampton?","1912")
+questao("Qual é o título do primeiro filme de Carry On feito e lançado em 1958?","Sargento")
+questao("Qual é o nome da maior empresa de tecnologia da Coréia do Sul?","Samsung")
+questao("Qual cantor liderou o grupo pop dos anos 1970 Showaddywaddy?","Dave Bartram")
+questao("Qual agora famoso chef de TV começou a cozinhar aos oito anos de idade no pub de seus pais, 'The Cricketers, em Clavering, Essex?","Jamie Oliver")
+questao("Qual jogador de dardos holandês venceu o Campeonato do Mundo BDO de 2012 no Lakeside Country Club, Frimley Green, em 15 de janeiro?","Kist Cristão")
+questao("Qual metal foi descoberto por Hans Christian Oersted em 1825?","alumínio")
+questao("Qual é a capital de Portugal?","Lisboa")
+questao("Quantas respirações o corpo humano toma diariamente?","20,000 diariamente")
+questao("Quem foi o primeiro-ministro da Grã-Bretanha de 1841 a 1846?","Robert Peel")
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Pequenos Projetos
 
 ## 1. Python
-### 1.1 Quizz
+### 1.1 Quizz :
+#### Um Quizz Simples que recebe algumas questões + respostas e imprime no terminal um texto especifico dependendo de se o usuário acertou ou não.
 ### 1.2 Chute um Número
 ### 1.3 Pedra, Papel e Tesoura
 ### 1.4 Gerenciador de Senhas
