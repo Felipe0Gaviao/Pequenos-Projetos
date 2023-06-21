911cfc43c9c05c2b6cfa5f174ba539a1ae28bad5
BatataSteve <batatasteve304@gmail.com>
2023-06-20 23:10:31 -0300

    Segundo Projeto

---
+++ Python/Chute um Numero/CuN.py
@@ -0,0 +1,22 @@
+import random
+
+def digito(x:str):
+    try: x=int(x);x = digito(input("tem que ser um número maior que 0: ")) if x<=0 else int(x);return x
+    except: print(f"{x} não é um número"); x = digito(input("Tente novamente: "));return x
+
+numero_maximo = digito(input("digite um número: "))
+numero_aleatorio = random.randint(0,numero_maximo)
+tentativas = 0
+escolha = digito(input(f"Chute um número entre 0 e {numero_maximo}: "))
+
+while escolha != numero_aleatorio:
+    tentativas += 1
+    print(f"Errou!")
+    print(f"Dica: você está acima do número" if escolha>numero_aleatorio else f"Dica: você está abaixo do número")
+
+    escolha = digito(input("Tente novamente: "))
+else:
+    print(f"Parabéns, você acertou")
+    print(f"você tentou chutar {tentativas} vez antes de acertar" if tentativas==1 else f"você tentou chutar {tentativas} vezes antes de acertar")
+
+#já volto
