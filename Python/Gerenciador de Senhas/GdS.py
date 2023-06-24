ec19a0c3be11232bdf71b56c94ed4011b32076b5
BatataSteve <batatasteve304@gmail.com>
2023-06-24 18:34:20 -0300

    final

--- Python/Gerenciador de Senhas/GdS.py
+++ Python/Gerenciador de Senhas/GdS.py
@@ -1,21 +1,37 @@
-#senha_mestra = input("Qual é a senha? ")
+from cryptography.fernet import Fernet
+
+def criar_chave():
+    chave = Fernet.generate_key()
+    with open("Python\Gerenciador de Senhas\chave.key", "wb") as arc_chave:
+        arc_chave.write(chave)
+
+def carregar_chave():
+    arc = open("Python\Gerenciador de Senhas\chave.key", "rb")
+    ch = arc.read()
+    arc.close
+    return ch
+
+
+chave_m = carregar_chave()
+fer = Fernet(chave_m)
+
 
 def ver():
 
     print("abrindo arquivo...")
-    with open('contas.txt','r') as file:
+    with open('Python\Gerenciador de Senhas\contas.txt','r') as file:
         for lines in file.readlines():
             info = lines.rstrip('\n')
-            us,sn = info.split(' | ')
-            print(f"{us}, {sn}")
+            us,sen = info.split(' | ')
+            print(f"Conta: {us}, Senha: {fer.decrypt(sen.encode()).decode()}")
 
 def add():
-    nome = input("Nome da conta:\n")
-    senha = input("Senha:\n")
+    us = input("Nome da conta:\n")
+    sn = input("Senha:\n")
 
     print("adicionando...")
-    with open('contas.txt','a') as file:
-        file.write(f"Nome: {nome} | Senha: {senha}\n")
+    with open('Python\Gerenciador de Senhas\contas.txt','a') as file:
+        file.write(f"{us} | {fer.encrypt(sn.encode()).decode()}\n")
     print("adicionando!")
     
 
