881e43db6f4422924065be51cf3a4cb361bdb1eb
BatataSteve <batatasteve304@gmail.com>
2023-08-16 22:11:23 -0300

    V2

--- Python/Jogo do Porquinho/JdP.py
+++ Python/Jogo do Porquinho/JdP.py
@@ -2,103 +2,74 @@ from random import randint,choice
 from itertools import cycle
 from time import sleep
 
-def roll():
-    return randint(1,6)
-
-def limpar_input(texto:str):
+def str_int(texto:str):
     try:
         texto = int(texto)
-        return texto
     except ValueError:
-        texto = limpar_input(input('Tem que ser um número: '))
-        return texto
+        texto = str_int(input('Tem que ser um número: '))
+    return texto
 
 print('Bem vindo ao jogo do Porquinho')
 
 OBJETIVO = 10
+REPR = {'j':'Jogador','c':'Computador'}
 
-pessoas = limpar_input(input('Quantas pessoas vão jogar? '))
-jogadores = {f'j{i+1}':0 for i in range(pessoas)}
-computadores = limpar_input(input('Quantos Computadores? '))
-jogadores.update({f'c{i+1}':0 for i in range(computadores)} if computadores>0 else {})
+pessoas = str_int(input('Quantas pessoas vão jogar? '))
+jogadores = {f'j{i+1}':{'at':0,'md':0} for i in range(pessoas)}
+
+computadores = str_int(input('Quantos Computadores? '))
+jogadores.update({f'c{i+1}':{'at':0,'md':0} for i in range(computadores)} if computadores>0 else {})
 
 ciclo = cycle(jogadores)
 ciclo_val = next(ciclo)
 
-sup_arr = []
+def rolar_dado():
+    valor = randint(1,6)
+
+    global ciclo_val, ciclo
+    
+    if valor == 1:
+        print('Uma pena, caiu 1')
+        sleep(.2)
+        print('você perdeu os pontos da rodada atual')
+        jogadores[ciclo_val]['md'] = jogadores[ciclo_val]['at']
+        sleep(.2)
+        print('passando para o próximo jogador...')
+        ciclo_val = next(ciclo)
+    else:
+        jogadores[ciclo_val]['md'] += valor
+        print(f'{REPR[ciclo_val[0]]} {ciclo_val[1]} está com {jogadores[ciclo_val]["md"]} nesta rodada')
+
+def passar_rodada():
+
+    global ciclo_val, ciclo
+
+    print('Mantendo Pontos Atuais...')
+    jogadores[ciclo_val]['at'] = jogadores[ciclo_val]['md']
+
+    print(f'{REPR[ciclo_val[0]]} {ciclo_val[1]} possui: {jogadores[ciclo_val]["at"]} {"ponto" if jogadores[ciclo_val]["at"] == 1 else "pontos"}')
+
+    print('passando para o próximo jogador...')
+    ciclo_val = next(ciclo)
 
 while True:
-    if ciclo_val[0] == 'p':
+    if ciclo_val[0] == 'j':
+
         print('Comandos: \
             \nr | roll | rolar : rolar o dado \
-            \np | pass | passar : passar para o próximo.')
-        
-        print(f'OBJETIVO: o jogo acaba quando alguém conseguir {OBJETIVO} pontos')
+            \np | pass | passar : passar para o próximo.\n')
+
+        print(f'OBJETIVO: o jogo acaba quando alguém conseguir {OBJETIVO} pontos\n')
 
         match input(f'Jogador {ciclo_val[1]}: '):
-            case c if c.lower() in ['r','roll','rolar']:
-
-                sup_arr.append(roll())
-                print(f'rolou: {sup_arr[-1]}')
-
-                if 1 in sup_arr:
-                    sup_arr = []
-                    print('Passando para o próximo jogador')
-
-                    ciclo_val = next(ciclo)
-                    continue
-                
-            case c if c.lower() in ['p','pass','passar']:
-                sum = 0
-                for i in sup_arr:
-                    sum+=i
-
-                jogadores[ciclo_val] += sum
-
-                print(f'Jogador {ciclo_val[1]} tem atualmente {jogadores[ciclo_val]}')
-
-                sup_arr = []
-
-                ciclo_val = next(ciclo)
-            case _:
-                print('Não é uma Opção Válida')
-                continue
+            case i if i.lower() in ['r','roll','rolar']:
+                rolar_dado()
+            case i if i.lower() in ['p','pass','passar']:
+                passar_rodada()
     if ciclo_val[0] == 'c':
-        escolha = choice(['r','p'])
         sleep(1)
-        escolhas = {'r':'rolar o dado','p':'passar para o próximo'}
+        choice([rolar_dado(),passar_rodada()])
 
-        print(f'Computador {ciclo_val[1]} escolheu {escolhas[escolha]}')
-        sleep(1)
-        match escolha:
-            case 'r':
-                sup_arr.append(roll())
-                print(f'rolou: {sup_arr[-1]}')
-
-                if 1 in sup_arr:
-                    sup_arr = []
-                    print('Passando para o próximo jogador')
-
-                    ciclo_val = next(ciclo)
-                    continue
-            case 'p':
-                sum = 0
-                for i in sup_arr:
-                    sum+=i
-
-                jogadores[ciclo_val] += sum
-
-                print(f'Computador {ciclo_val[1]} tem atualmente {jogadores[ciclo_val]}')
-
-                sup_arr = []
-
-                ciclo_val = next(ciclo)
-
-    valor = any(v >= OBJETIVO for v in jogadores.values())
-    
-    if valor:
-        print('checando')
-        for c,v in jogadores.items():
-            if v >= OBJETIVO:
-                print(f'{"Jogador" if c[0] == "p" else "Computador"} {c[1]} Ganhou o Jogo')
-                break
+    if jogadores[ciclo_val]['at'] >= OBJETIVO:
+        print(f'{REPR[ciclo_val[0]]} {ciclo_val[1]} Ganhou o Jogo')
+        break
